47b4e1e5dce87d49ad244247a86e98433531eef0
slinky55 <larry.mason@ufl.edu>
2023-05-27 12:31:12 -0400

    added api packge, added gin as a dependancy

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 		"fmt"
+		"github.com/gin-gonic/gin"
+
+		"github.com/slinky55/emfic/api"
 )
 
 func main() {
 		fmt.Println("Hello, World!")
+		
+		r := gin.Default()
+		
+		g_api := r.Group("/api")
+		{
+				g_api.GET("/ping", api.Ping)		
+		}
+
+		r.Run(":7100")
 }
